1b373fa801ff906629046ccea3adecdef5b2b30f
rajnish0218 <113040515+rajnish0218@users.noreply.github.com>
2022-10-06 20:36:52 +0530

    Add files via upload

---
+++ Lab 06-10-22/Practical1/Person.java
@@ -0,0 +1,26 @@
+package Practical1;
+
+public class Person {
+	
+	void title1()
+	{
+		System.out.println("---Student Details---");
+	}
+	
+	void title2()
+	{
+		System.out.println("---Employees Details---");
+	}
+	
+	
+	void person_ID()
+	{
+		System.out.println("person_ID");
+	}
+	
+	void person_name()
+	{
+		System.out.println("person_Name");
+	}
+
+}
