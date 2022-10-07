4f584a054834918ab474b2568de901effdc67b51
rajnish0218 <113040515+rajnish0218@users.noreply.github.com>
2022-10-07 14:37:50 +0530

    Add files via upload

---
+++ Lab 06-10-22/Practical3/MethodOverloading.java
@@ -0,0 +1,17 @@
+package Practical3;
+
+public class MethodOverloading {
+
+	
+	void addition( int x,int y)
+	{
+		int result = x+y;
+		System.out.println(result);
+	}
+	
+	void addition (double x, double y)
+	{
+		double result = x+y;
+		System.out.println(result);
+	}
+}
---
+++ Lab 06-10-22/Practical3/MethodOverloading_Main.java
@@ -0,0 +1,13 @@
+package Practical3;
+
+public class MethodOverloading_Main {
+
+	public static void main(String[] args) {
+		
+		MethodOverloading m = new MethodOverloading();
+		m.addition(5, 5);
+		m.addition(4.5, 4.5);
+
+	}
+
+}
